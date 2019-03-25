59886d923cc037bb26f14f57a57e6ae670ee9adb
Tom Coemans <tom.coemans@gmail.com>
2019-03-25 18:31:09 +0100

    WEBC-11 vl-icon.js verwijderd en ignore

--- vl-icon.js
+++
@@ -1,6 +0,0 @@
-import{VlElement}from"./node_modules/vl-ui-core/vl-core.src.js";export class VlIcon extends VlElement(HTMLElement){static get _observedAttributes(){return["icon","size"]}static get _observedChildClassAttributes(){return["before","after"]}constructor(){super(`
-            <style>
-                @import "../style.css";
-            </style>
-            <span class="vl-icon vl-vi" aria-hidden="true"></span>
-        `)}get _classPrefix(){return"vl-icon--"}_iconChangedCallback(oldValue,newValue){this._changeClass(this._element,oldValue,newValue,"vl-vi-")}_sizeChangedCallback(oldValue,newValue){if(["small","large"].indexOf(newValue)>=0){this._changeClass(this._element,oldValue,newValue)}else{this._element.classList.remove(this._prefix+oldValue)}}};customElements.define("vl-icon",VlIcon);
