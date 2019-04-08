77520f03bc6f6c45d2ace54031d2b92d3adc36cd
Tom Coemans <tom.coemans@gmail.com>
2019-04-08 15:40:30 +0200

    WEBC-4 documentatie aangevuld

--- vl-icon.src.js
+++ vl-icon.src.js
@@ -3,6 +3,14 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.src.js';
 /**
  * vl-icon
  * De wrapper class "vl-icon-wrapper" wordt NIET in deze Web Component geïmplementeerd
+ * ### Attributen
+ * Attribuut | Uitleg | Waarde
+ * ----------|--------|--------
+ * `icoon` | Wordt gebruikt om aan te geven welk icoon getoond moet worden. | { string }
+ * `size` | Wordt gebruikt om het icoon te verkleinen (80%) of te vergroten (120%) ten opzichte van de parent. | { string } small - large
+ * `ligt`| Wordt gebruikt om het icoon een lichte kleur te geven? | { boolean }
+ * `before` | Wordt gebruikt wanneer het icoon voor een tekst staat en er wat ruimte tussen het icoon en de tekst getoond moet worden. | { boolean }
+ * `after` | Wordt gebruikt wanneer het icoon achter een tekst staat en er wat ruimte tussen het icoon en de tekst getoond moet worden. | { boolean }
  *
  * @demo demo/vl-icon.html
  */
