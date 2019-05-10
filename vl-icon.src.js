109295a781e655533fb7d16fd5db7a1289585adb
Tom Coemans <tom.coemans@gmail.com>
2019-05-10 16:56:26 +0200

    WEBC-83 testen toegevoegd

--- vl-icon.src.js
+++ vl-icon.src.js
@@ -7,7 +7,7 @@ import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
  * 
  * @extends NativeVlElement
  * 
- * @property {string} icoon - Attribuut wordt gebruikt om aan te geven welk icoon getoond moet worden.
+ * @property {string} icon - Attribuut wordt gebruikt om aan te geven welk icoon getoond moet worden.
  * @property {(small | large)} size - Attribuut wordt gebruikt om het icoon te verkleinen (80%) of te vergroten (120%) ten opzichte van de parent.
  * @property {boolean} light - Attribuut wordt gebruikt om het icoon een lichte kleur te geven.
  * @property {boolean} before - Attribuut wordt gebruikt wanneer het icoon voor een tekst staat en er wat ruimte tussen het icoon en de tekst getoond moet worden.
