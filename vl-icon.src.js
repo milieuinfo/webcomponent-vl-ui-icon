4e5eca8e3598b2e5fcafddfbd093c05e9e67eda6
Tom Coemans <tom.coemans@gmail.com>
2019-05-10 15:35:26 +0200

    WEBC-83 link attribuut toegevoegd

--- vl-icon.src.js
+++ vl-icon.src.js
@@ -17,7 +17,7 @@ import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
  */
 export class VlIcon extends VlElement(HTMLElement) {
     static get _observedAttributes() {
-        return ['icon', 'size', '90deg', '180deg'];
+        return ['icon', 'size', '90deg', '180deg', 'link'];
     }
 
     static get _observedChildClassAttributes() {
@@ -59,6 +59,18 @@ export class VlIcon extends VlElement(HTMLElement) {
 
     _180degChangedCallback(oldValue, newValue) {
         this._toggleClass(this._element, newValue, 'vl-vi-u-180deg');
+    }
+
+    _linkChangedCallback(oldValue, newValue) {
+        if (newValue != undefined) {
+            this._element.classList.forEach((value) => {
+                this._element.classList.replace(value, value.replace('-icon', '-link__icon'));
+            });
+        } else {
+            this._element.classList.forEach((value) => {
+                this._element.classList.replace(value, value.replace('-link__icon', '-icon'));
+            });
+        }
     }
 }
 
