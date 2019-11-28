66bb8079c00f0946d5332007d0e49a55329a73ce
cambiph <incubusattax@gmail.com>
2019-11-28 15:14:27 +0100

    Voeg placeholder testen toe Voeg pagina-object toe

--- test/e2e/icon.test.js
+++ test/e2e/icon.test.js
@@ -9,9 +9,33 @@ describe('vl-icon', async () => {
         return vlIconPage.load();
     });
 
-    it('placeholder', async () => {
+    it('als gebruiker wil ik het verschil kunnen zien tussen een klein en een gewoon icon', async () => {
+        assert.isTrue(true);
+    });    
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een licht en een gewoon icon', async () => {
+        assert.isTrue(true);
+    });    
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een groot en een gewoon icon', async () => {
+        assert.isTrue(true);
+    });    
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een icoon dat 90 graden gedraaid is en een gewoon icon', async () => {
+        assert.isTrue(true);
+    });   
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een icoon dat 180 graden gedraaid is en een gewoon icon', async () => {
+        assert.isTrue(true);
+    });    
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een icoon waar gedefinieerd is dat het voor een tekst staat en een gewoon icoon', async () => {
+        assert.isTrue(true);
+    });    
+    
+    it('als gebruiker wil ik het verschil kunnen zien tussen een icoon waar gedefinieerd is dat het achter een tekst staat en een gewoon icoon', async () => {
         assert.isTrue(true);
     });
-
+    
     after(() => driver && driver.quit());
 });
--- test/e2e/pages/vl-icon.page.js
+++ test/e2e/pages/vl-icon.page.js
@@ -7,6 +7,38 @@ class VlIconPage extends Page {
         return new VlIcon(this.driver, selector);
     }
 
+    async getSmallIcon() {
+        return this._getIcon('#icon-small');
+    }
+
+    async getLightIcon() {
+        return this._getIcon('#icon-light');
+    }
+
+    async getLargeIcon() {
+        return this._getIcon('#icon-large');
+    }
+
+    async getIcon90() {
+        return this._getIcon('#icon-90');
+    }
+
+    async getIcon180() {
+        return this._getIcon('#icon-180');
+    }
+
+    async getIconBefore() {
+        return this._getIcon('#icon-before');
+    }
+
+    async getIconAfter() {
+        return this._getIcon('#icon-after');
+    }
+
+    async getIconAfterFlex() {
+        return this._getIcon('#icon-after-flex');
+    }
+
     async load() {
         await super.load(Config.baseUrl + '/demo/vl-icon.html');
     }
