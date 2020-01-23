1cc89387d06c1bd0940d1dc2ed6083344dc4a51a
Tom Coemans <tom.coemans@gmail.com>
2020-01-23 17:46:04 +0100

    module export

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { VlIcon } = require('./vl-icon.src.js');
 
 module.exports = { 
-    VlIcon
+    VlIcon: require('./vl-icon.src.js'),
+    Test: {
+        VlIcon: require('./test/e2e/components/vl-icon.js')
+    }
 }
