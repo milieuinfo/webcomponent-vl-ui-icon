9a55ba1bb286387dfea0bc259c8f93bbd85c6469
Tom Coemans <tom.coemans@gmail.com>
2020-01-26 10:57:46 +0100

    src code niet exporteren

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const { VlIcon } = require('./vl-icon.src.js');
-
 module.exports = { 
-    VlIcon: require('./vl-icon.src.js'),
     Test: {
         VlIcon: require('./test/e2e/components/vl-icon.js')
     }
