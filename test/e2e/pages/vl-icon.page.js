000bbd6f4810c1cec5e8af0577665984c74152e6
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 13:32:31 +0200

    ESlint

--- test/e2e/pages/vl-icon.page.js
+++ test/e2e/pages/vl-icon.page.js
@@ -1,50 +1,50 @@
 const VlIcon = require('../components/vl-icon');
-const { Page, Config } = require('vl-ui-core').Test;
+const {Page, Config} = require('vl-ui-core').Test;
 
 class VlIconPage extends Page {
-    async _getIcon(selector) {
-        return new VlIcon(this.driver, selector);
-    }
+  async _getIcon(selector) {
+    return new VlIcon(this.driver, selector);
+  }
 
-    async getIcon() {
-        return this._getIcon('#icon');
-    }
+  async getIcon() {
+    return this._getIcon('#icon');
+  }
 
-    async getSmallIcon() {
-        return this._getIcon('#icon-small');
-    }
+  async getSmallIcon() {
+    return this._getIcon('#icon-small');
+  }
 
-    async getLightIcon() {
-        return this._getIcon('#icon-light');
-    }
+  async getLightIcon() {
+    return this._getIcon('#icon-light');
+  }
 
-    async getLargeIcon() {
-        return this._getIcon('#icon-large');
-    }
+  async getLargeIcon() {
+    return this._getIcon('#icon-large');
+  }
 
-    async getIcon90() {
-        return this._getIcon('#icon-90');
-    }
+  async getIcon90() {
+    return this._getIcon('#icon-90');
+  }
 
-    async getIcon180() {
-        return this._getIcon('#icon-180');
-    }
+  async getIcon180() {
+    return this._getIcon('#icon-180');
+  }
 
-    async getIconBefore() {
-        return this._getIcon('#icon-before');
-    }
+  async getIconBefore() {
+    return this._getIcon('#icon-before');
+  }
 
-    async getIconAfter() {
-        return this._getIcon('#icon-after');
-    }
+  async getIconAfter() {
+    return this._getIcon('#icon-after');
+  }
 
-    async getIconAfterFlex() {
-        return this._getIcon('#icon-after-flex');
-    }
+  async getIconAfterFlex() {
+    return this._getIcon('#icon-after-flex');
+  }
 
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-icon.html');
-    }
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-icon.html');
+  }
 }
 
 module.exports = VlIconPage;
