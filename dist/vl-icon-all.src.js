c2ba0a8a38a922fd8ffc87619b8d691cc6a5b430
coemans <tom.coemans@vlaanderen.be>
2021-02-01 20:33:17 +0100

    update vl-ui-core dependency (#98)

--- dist/vl-icon-all.src.js
+++ dist/vl-icon-all.src.js
@@ -1,5 +1,5 @@
-import {VlIcon} from 'vl-ui-icon/dist/vl-icon.src.js';
-import {VlIconWrapper} from 'vl-ui-icon/dist/vl-icon-wrapper.src.js';
+import {VlIcon} from '../dist/vl-icon.src.js';
+import {VlIconWrapper} from '../dist/vl-icon-wrapper.src.js';
 
 export {
   VlIcon,
